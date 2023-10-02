ade1c2f9fb611b553a7f854f62c6615bd844c3a6
END <khoadeptrai07@gmail.com>
2023-10-02 20:07:13 +0700

    Them bool

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string b = "ABC";
-Console.WriteLine(b);
+bool newbool = true;
+Console.WriteLine(newbool);
