c06a58ee93ecdb8c5cf04774b3dfd9cc716e31c1
hoa.bui <hoa.bui@saigontechnology.com>
2023-10-02 21:16:02 +0700

    Them ham kiem tra SNT

--- Program.cs
+++ Program.cs
@@ -1,5 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-bool newbool = true;
-Console.WriteLine(newbool);
-Console.ReadLine();
+﻿public class Programm
+{
+    static void Main(string[] args)
+    {
+        int a = 6;
+        string a1 = "6";
+        bool isSoNguyenTo = CheckSoNguyenTo(a);
+        // float double
+        double ketqua = Math.Sqrt(9);
+        Console.WriteLine(ketqua);
+        if (isSoNguyenTo)
+        {
+            Console.WriteLine("{0} la so nguyen to", a);
+        }
+        else
+        {
+            Console.WriteLine("{0} khong la so nguyen to", a);
+        }
+    }
+
+    static bool CheckSoNguyenTo(int number)
+    {
+        for (int i = 2; i <= number / 2; i++)
+        {
+            if (CheckChiaHet(number, i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static bool CheckChiaHet(int a123, int b)
+    {
+        return a123 % b == 0;
+    }
+}
