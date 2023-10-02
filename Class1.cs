98e2e852a3ddec91d54e891f9f2aad5b0617c813
END <khoadeptrai07@gmail.com>
2023-10-02 20:37:00 +0700

    Them lenh

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Thay doi :D");
+        }
+    }
+}
